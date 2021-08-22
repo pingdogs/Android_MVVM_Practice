1a8fe86218ed078120beec4e32ca1e169172e3cc
YuShin <77323868+pingdogs@users.noreply.github.com>
2021-08-22 16:13:19 +0800

    Delete app directory

--- app/src/main/java/com/example/myapplication/newsapiclient/data/dataSource/NewsRemoteDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.newsapiclient.data.dataSource
-
-import com.example.myapplication.newsapiclient.data.model.ApiResponse
-import retrofit2.Response
-
-interface NewsRemoteDataSource {
-    suspend fun  getTopHeadlines(country:String, page:Int): Response<ApiResponse>
-    suspend fun  getSearchTopHeadlines(country:String, searchQuery: String,  page:Int): Response<ApiResponse>
-}
